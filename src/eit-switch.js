37bf7996acc646b14a7cf60b557febcff5b1e52a
xavi.hewel@gmail.com <xavi.hewel@gmail.com>
2022-11-16 07:37:09 +0100

    first commit

---
+++ src/eit-switch.js
@@ -0,0 +1,11 @@
+import { LitElement, html } from "lit-element";
+
+class EitSwitch extends LitElement {
+
+    render() {
+        return html`
+        Soc el eit-switch!!
+        `;
+    }
+}
+customElements.define('eit-switch', EitSwitch);
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './eit-input';
+import './feedback-element';
+import './my-counter';
+import './menu-overlay'
